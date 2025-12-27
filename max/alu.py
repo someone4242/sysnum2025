135451d130e8120c80b4a66686c6928fc679dcbc
Max Holemans <holemans.max@gmail.com>
2025-12-27 11:35:45 +0100

    Ajout de max/alu.py : ALU qui semble être OK
    Implémentation de l'ALU en 32 bits (ou tout N = 2^k) avec :
    - Opérations ADD, SUB, AND, XOR, OR, NOT
    - Flags C (carry), V (overflow), N (negative), Z (zero)
    - Nadder en Carry-lookahead (vu en TD)
    - Netlist : ~1000 lignes

---
+++ max/alu.py
@@ -0,0 +1,105 @@
+
+from lib_carotte import *
+
+N_exp = 5
+N = 2**N_exp
+# 32-bit integers (signed or unsigned two-complement)
+
+# Utils
+
+def bus_unfold_def(bus_size, lam):
+    bus = lam(0)
+    for i in range(1, bus_size):
+        bus = bus + lam(i)
+    return bus
+
+def multi_And(var_list):
+    if len(var_list) == 1:
+        return var_list[0]
+    n = len(var_list) // 2
+    return And(multi_And(var_list[:n]), multi_And(var_list[n:]))
+
+def multi_Or(var_list):
+    if len(var_list) == 1:
+        return var_list[0]
+    n = len(var_list) // 2
+    return Or(multi_Or(var_list[:n]), multi_Or(var_list[n:]))
+
+def is_null(bus):
+    if bus.bus_size == 1:
+        return ~bus
+    n = bus.bus_size // 2
+    return is_null(bus[:n]) & is_null(bus[n:])
+
+# Carry-Lookahead Adder
+
+def gen_prop_1_bit_adder(a, b, c):
+    return (a^b^c, a^b, a&b) # R, P, G
+
+def nadder(A, B, C0):
+
+    assert A.bus_size == N and B.bus_size == N
+
+    def carry_lookahead(height, a, b, c):
+        if height == 0:
+            r, p, g = gen_prop_1_bit_adder(a, b, c)
+            return r, p, g, p, g
+        n = len(a)//2
+        r0, p0, g0, xor_bus0, and_bus0 = carry_lookahead(
+                height -1, a[0:n], b[0:n], c)
+        c1 = g0 | (p0 & c)
+        r1, p1, g1, xor_bus1, and_bus1 = carry_lookahead(
+                height -1, a[n:2*n], b[n:2*n], c1)
+
+        r, p, g = (r0+r1), (p0 & p1), (g1 | (g0 & p1))
+        xor_bus, and_bus = xor_bus0 + xor_bus1, and_bus0 + and_bus1
+        return r, p, g, xor_bus, and_bus
+
+    R, P, G, X, A = carry_lookahead(N_exp, A, B, C0)
+    return R, (G | (P & C0)), X, A
+
+# ALU
+
+def ALU(A, B, Sub_inp, Xor_inp, And_inp, Or_inp, Not_inp):
+    nadder_A = A
+    nadder_B = bus_unfold_def(N, lambda i : (B[i] ^ Sub_inp) | Not_inp)
+    nadder_X = Sub_inp
+
+    nadder_S, nadder_C, nadder_XOR, nadder_AND = nadder(
+            nadder_A, nadder_B, nadder_X)
+
+    arith_out = multi_And([~Xor_inp, ~And_inp, ~Or_inp, ~Not_inp])
+    S = bus_unfold_def(N, lambda i : multi_Or([
+            arith_out & nadder_S[i],
+            And_inp & nadder_AND[i],
+            (Xor_inp | Not_inp) & nadder_XOR[i],
+            Or_inp & (nadder_AND[i] | nadder_XOR[i])
+        ]))
+
+    flag_C = nadder_C
+    flag_V = And( ~(Xor(A[N-1],B[N-1])), Xor(A[N-1], S[N-1]))
+    flag_N = S[N-1]
+    flag_Z = is_null(S)
+
+    return S, flag_C, flag_V, flag_N, flag_Z
+
+
+def main():
+    A, B = Input(N, "A"), Input(N, "B")
+    Sub_inp = Input(1, "Sub")
+    Xor_inp = Input(1, "Xor")
+    And_inp = Input(1, "And")
+    Or_inp = Input(1, "Or")
+    Not_inp = Input(1, "Not")
+
+    S, C, flag_V, flag_N, flag_Z = ALU(
+            A, B,
+            Sub_inp, Xor_inp, And_inp, Or_inp, Not_inp)
+
+    S.set_as_output("S")
+    C.set_as_output("C")
+    flag_V.set_as_output("V")
+    flag_N.set_as_output("N")
+    flag_Z.set_as_output("Z")
+
+
