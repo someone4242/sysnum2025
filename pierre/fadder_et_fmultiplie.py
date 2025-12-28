b7069fd454cddc99d04847f93dd6431e87ee136e
pingual <mrpierrebodet@gmail.com>
2025-12-28 12:27:14 +0000

    Update fadder_et_fmultiplie.py

--- pierre/fadder_et_fmultiplie.py
+++ pierre/fadder_et_fmultiplie.py
@@ -65,12 +65,12 @@ def fadd(a: Variable, b: Variable) -> Variable:
     return Mux(condition, fadd_temp(b,a), fadd_temp(a,b))
 
 def retire_zeros_gauche(mantisse: Variable, exposant: Variable) -> typing.Tuple[Variable, Variable]:
-    compteur = Constant("0"*exposant.bus_size)
-    a_trouve_un = Constant("0")
+    exposant = adder(exposant,Constant("0"*exposant.bus_size),mantisse[21])[0]
+    doit_sarreter = comparer.fegal_zero(mantisse[10:])
     for i in range(mantisse.bus_size):
-        compteur = adder(compteur,Constant("0"*compteur.bus_size),(~mantisse[i] & ~a_trouve_un))[0]
-        a_trouve_un = a_trouve_un | mantisse[i]
-    return (bitshift.ajouter_zeros_droite(mantisse,compteur), adder(exposant, compteur, Constant("0"))[0])
+        mantisse = Mux(doit_sarreter, mantisse[1:]+Constant("0"), mantisse)
+        doit_sarreter = doit_sarreter | comparer.fegal_zero(mantisse[10:])
+    return mantisse,exposant
 
 
 def fmultiplie(a: Variable, b: Variable) -> Variable:
@@ -78,19 +78,63 @@ def fmultiplie(a: Variable, b: Variable) -> Variable:
 
     signe_a, exposant_a, mantisse_a = encodedecode.fdecode(a)
     signe_b, exposant_b, mantisse_b = encodedecode.fdecode(b)
-
-    new_mantisse = multiplie.multiplier_recursive(mantisse_a+Constant("00000"),mantisse_b+Constant("00000"))
+    exposant_a = exposant_a + Constant("00000000000")
+    exposant_b = exposant_b + Constant("00000000000")
+    
+    new_mantisse = multiplie.multiplier_recursive(mantisse_a+Constant("0"*(a.bus_size-mantisse_a.bus_size)),mantisse_b+Constant("0"*(b.bus_size-mantisse_b.bus_size)))
     new_exposant = adder(exposant_a,exposant_b,Constant("0"))[0]
+    new_exposant = adder(new_exposant,inverse_bits(encodedecode.biais()+Constant("0"*(new_exposant.bus_size-encodedecode.biais().bus_size))),Constant("1"))[0]
     new_signe = signe_a ^ signe_b
-
     new_mantisse, new_exposant = retire_zeros_gauche(new_mantisse,new_exposant)
 
-    return encodedecode.fencode(new_signe,new_exposant,new_mantisse[:11])
+    return encodedecode.fencode(new_signe,new_exposant[:5],new_mantisse[:11])
 
+
+def mantisse_divise(a: Variable, b: Variable) -> Variable:
+    # On augmente la taille de 1 pour pouvoir gérer les cas où a < b
+    a = a + Constant("0")
+    b = b + Constant("0")
+
+    taille = b.bus_size
+    moins_b = inverse_bits(b)
+
+    condition = comparer.plus_grand_large(a,b)
+    q = condition
+    a =  Mux(condition, a, adder(a,moins_b,Constant("1"))[0])
+    a = Constant("0") + a[:taille-1] # bitshift
+
+    for _ in range(a.bus_size-2):
+        condition = comparer.plus_grand_large(a,b)
+        q = condition + q
+        a =  Mux(condition, a, adder(a,moins_b,Constant("1"))[0])
+        a = Constant("0") + a[:taille-1] # bitshift
+    return q
+
+def fdivise(a: Variable, b: Variable) -> Variable:
+    assert a.bus_size == b.bus_size
+
+    signe_a, exposant_a, mantisse_a = encodedecode.fdecode(a)
+    signe_b, exposant_b, mantisse_b = encodedecode.fdecode(b)
+
+    new_mantisse = mantisse_divise(mantisse_a,mantisse_b)
+    new_exposant = adder(exposant_a,encodedecode.biais()+Constant("0"*(exposant_a.bus_size-encodedecode.biais().bus_size)),Constant("0"))[0]
+    new_exposant = adder(new_exposant,inverse_bits(exposant_b),Constant("1"))[0]
+    new_signe = signe_a ^ signe_b
+    #return mantisse_a,mantisse_b
+    #new_mantisse, new_exposant = retire_zeros_gauche(new_mantisse,new_exposant)
+
+    a_est_nan = Constant("1")
+    b_est_nan = Constant("1")
+    for i in range(exposant_a.bus_size):
+        a_est_nan = a_est_nan & exposant_a[i]
+        b_est_nan = b_est_nan & exposant_b[i]
+    
+    return Mux(a_est_nan | b_est_nan | comparer.fegal_zero(b), encodedecode.fencode(new_signe,new_exposant,new_mantisse[:11], encodedecode.nan()))
 
 def main() -> None:
     n = 16
     a = Input(n)
     b = Input(n)
-    r = fmultiplie(a,b)
+    #r = fmultiplie(a,b)
+    r = fdivise(a,b)
     r.set_as_output("z")
