45c5d43ca888dd866e790fe33c6818463c8ef956
someone4242 <dai.charles.ym@gmail.com>
2026-01-26 11:48:07 +0100

    ajout de lw et sw

--- charles/carotte/processeur/processeur.py
+++ charles/carotte/processeur/processeur.py
@@ -1,6 +1,8 @@
 from lib_carotte import *
 from alu import *
 
+ram_addr_size = 8
+ram_size = 2**ram_addr_size
 pc_size = 8
 opcode_len = 5
 reg_desc_size = 5
@@ -13,38 +15,37 @@ one_8bit = Constant("1" + ("0" * (pc_size - 1)))
 zero = false = Constant("0")
 val = [zero, one]
 
-# sub, xor, and, or, not, sll, srl, mul, i_src, RegWrite, MemWrite, jmp, ALU src, branch
+# sub, xor, and, or, not, sll, srl, mul, i_src, RegWrite, MemWrite, jmp, MemRead, branch
 
 str_signal = [
-    "00000000" + "010010", # add  
-    "00000000" + "110010", # addi 
-    "00100000" + "010010", # and  
-    "00100000" + "110010", # andi 
-    "00010000" + "010010", # or   
-    "00010000" + "110010", # ori  
-    "01000000" + "010010", # xor  
-    "01000000" + "110010", # xori 
-    "10000000" + "010010", # sub  
-    "0" * 14, # dummy
-    "00000010" + "010010", # sll TO check
-    "00000010" + "110010", # slli TO check
-    "00000100" + "010010", # srl TO check
-    "00000100" + "110010", # srli TO check
-    "00000001" + "010010", # mul TO check
-    "0" * 14, # dummy
-    "00000000" + "010110", # jal TO check
-    "10000000" + "010011", # beq 
-    "10000000" + "010011", # bne 
-    "10000000" + "010011", # blt 
-    "10000000" + "010011", # bge 
-    "00000000" + "010010", # fadd TODO
-    "00000000" + "010010", # fsub TODO
-    "00000000" + "010010", # fmul TODO
-    "00000000" + "010010", # fdiv TODO
-    "00000000" + "010010", # ffisqrt TODO
-    "00000000" + "010010", # feq TODO
-    "00000000" + "010000", # load 
-    "00000000" + "001000", # store 
+    "00000000" + "010000", # add  
+    "00000000" + "110000", # addi 
+    "00100000" + "010000", # and  
+    "00100000" + "110000", # andi 
+    "00010000" + "010000", # or   
+    "00010000" + "110000", # ori  
+    "01000000" + "010000", # xor  
+    "01000000" + "110000", # xori 
+    "10000000" + "010000", # sub  
+    "00000000" + "001000", # sw TO check
+    "00000010" + "010000", # sll
+    "00000010" + "110000", # slli
+    "00000100" + "010000", # srl
+    "00000100" + "110000", # srli
+    "00000001" + "010000", # mul 
+    "00000000" + "010010", # lw TO check
+    "00000000" + "010100", # jal TO check
+    "10000000" + "010001", # beq 
+    "10000000" + "010001", # bne 
+    "10000000" + "010001", # blt 
+    "10000000" + "010001", # bge 
+    "00000000" + "010010", # rdtime TODO
+    "00000000" + "010000", # fadd TODO
+    "00000000" + "010000", # fsub TODO
+    "00000000" + "010000", # fmul TODO
+    "00000000" + "010000", # fdiv TODO
+    "00000000" + "010000", # ffisqrt TODO
+    "00000000" + "010000", # feq TODO
     "00000000" + "000100", # jmp
     "00000000" + "000100"  # jz
 ]
@@ -88,23 +89,49 @@ def main():
     reg = [RAM(1, reg_size, zero, true, zero, Defer(reg_size, lambda i=i: mov_to_reg[i])) for i in range(2**reg_desc_size)]
     reg[0] = Constant("0" * word_size)
 
+    # lecture de l'instruction et préparation des signaux
     instr = ROM(pc_size, instr_size, pc)
-    instr.set_as_output("instruction")
-
     signal_tree = mux_tree(instr[1:1+opcode_len], opcode_len, ctrl_signal)
-    sub_alu, xor_alu, and_alu, or_alu, not_alu, sll_alu, srl_alu, mul_alu, isrc, reg_write, mem_write, jmp, alu_sr, branch = signal_tree[1]
-
+    sub_alu, xor_alu, and_alu, or_alu, not_alu, sll_alu, srl_alu, mul_alu, isrc, reg_write, mem_write, jmp, mem_read, branch = signal_tree[1]
     imm_i = Concat(instr[20:32], Constant("0"*20))
     imm_s = Concat(instr[7:12], instr[25:32])[0:pc_size]
     reg_dest = instr[7:12]
     reg_src1 = instr[15:20]
     reg_src2 = instr[20:25]
     jmp_offset = instr[12:12+pc_size]
+    mem_offset = Concat(instr[20:32], Constant("0"*20))
+    rs1_value = mux_tree(reg_src1, reg_desc_size, reg)[1]
+    rs2_value = mux_tree(reg_src2, reg_desc_size, reg)[1]
+    A = rs1_value
+    B = Mux(isrc, rs2_value, imm_i)
+    mem_addr, n, z, c, v = ALU(5, A, mem_offset, false, false, false, false, false, false, false, false)
+
+    # calcul de la mémoire 
+    mem_value = RAM(ram_addr_size, word_size, mem_addr[0:8], mem_write, mem_addr[0:8], rs2_value)
+
+    # calcul du résultat de l'ALU et des flags
+    write_enable = demux_tree(reg_dest, reg_desc_size, [reg_write])
+    ALU_res, C, V, N, Z = ALU(5, A, B, sub_alu, xor_alu, and_alu, or_alu, not_alu, sll_alu, srl_alu, mul_alu)
+    E, LT = Z, N 
+    NE, GE = ~E, ~LT
+
+    #calcul du program counter
+    #condition = mux_tree(instr[1:3], 2, [NE, LT, GE, E])
+    pc_incr, c, v, n, z = ALU(3, pc, one_8bit, false, false, false, false, false, false, false, false)
+    condition = Mux(instr[2], Mux(instr[1], GE, E), Mux(instr[1], NE, LT))
+    pc_offset = Mux(branch & condition, Mux(jmp, one_8bit, jmp_offset), imm_s)
+    next_pc, c, v, n, z = ALU(3, pc, pc_offset, false, false, false, false, false, false, false, false)
+
+
+    # calcul de mov_value
+    mov_value = Mux(mem_read, mem_value, Mux(jmp, ALU_res, Concat(pc_incr, Constant("0"*(reg_size - pc_size)))))
+    mov_to_reg = [Mux(write_enable[i], reg[i], mov_value) for i in range(reg_nb)]
+
+
+    instr.set_as_output("instruction")
     reg_src1.set_as_output("rs1")
     reg_src2.set_as_output("rs2")
-    A = mux_tree(reg_src1, reg_desc_size, reg)[1]
-    B = Mux(isrc, mux_tree(reg_src2, reg_desc_size, reg)[1], imm_i)
-
+    ALU_res.set_as_output("ALU_result")
     reg_dest.set_as_output("reg_dest")
     sub_alu.set_as_output("sub")
     xor_alu.set_as_output("xor")
@@ -119,26 +146,10 @@ def main():
     A.set_as_output("A")
     B.set_as_output("B")
 
-    write_enable = demux_tree(reg_dest, reg_desc_size, [reg_write])
-    ALU_res, C, V, N, Z = ALU(5, A, B, sub_alu, xor_alu, and_alu, or_alu, not_alu, sll_alu, srl_alu, mul_alu)
-    E, LT = Z, N 
-    NE, GE = ~E, ~LT
-
-    ALU_res.set_as_output("ALU_result")
-    pc_incr, c, v, n, z = ALU(3, pc, one_8bit, false, false, false, false, false, false, false, false)
-    mov_value = Mux(jmp, ALU_res, Concat(pc_incr, Constant("0"*(reg_size - pc_size))))
-
-    #condition = mux_tree(instr[1:3], 2, [NE, LT, GE, E])
-    condition = Mux(instr[2], Mux(instr[1], GE, E), Mux(instr[1], NE, LT))
-    pc_offset = Mux(branch & condition, Mux(jmp, one_8bit, jmp_offset), imm_s)
-    next_pc, c, v, n, z = ALU(3, pc, pc_offset, false, false, false, false, false, false, false, false)
-    mov_to_reg = [Mux(write_enable[i], reg[i], mov_value) for i in range(32)]
-
     pc.set_as_output("program_counter")
+
     for i in range(1, 9):
         reg[i].set_as_output("x" + str(i))
-    # for i in range(len(write_enable)):
-    #     write_enable[i].set_as_output("write_x" + str(i))
     all_write = concat(write_enable)
     all_write.set_as_output("write")
     reg_write.set_as_output("reg_write")
